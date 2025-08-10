8f5a2daf82e6798c9f7a909206d8a6e1cdc97e14
AhmadV9N <dompredo3@gmail.com>
2025-08-10 01:02:28 -0300

    Update Speed%20Hub%20X.lua

--- Speed%20Hub%20X.lua
+++ Speed%20Hub%20X.lua
@@ -1,7 +1,1 @@
-task.spawn(function()
-    loadstring(game:HttpGet("https://pastefy.app/AxwmbsYt/raw"))()
-end)
-
-task.spawn(function()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/Speed%20Hub%20X.lua", true))()
-end)
+loadstring(game:HttpGet("https://pastefy.app/AxwmbsYt/raw"))();loadstring(game:HttpGet("https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/Speed%20Hub%20X.lua", true))()
